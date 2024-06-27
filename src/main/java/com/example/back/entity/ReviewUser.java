8af221b68afacf0830ece1b84884c4613ee561ce
CPG248 <161663562+CPG248@users.noreply.github.com>
2024-06-27 14:45:24 +0900

    Qwer (#58)
    * review
    
    * review
    
    * test
    
    * review
    
    * productid=number
    
    * main 500error-fix

---
+++ src/main/java/com/example/back/entity/ReviewUser.java
@@ -0,0 +1,27 @@
+package com.example.back.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+//@Entity
+//public class ReviewUser {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    @ManyToOne
+//    @JoinColumn(name="review_number")
+//    private ReviewEntity likedReviewEntity;
+//    @ManyToOne
+//    @JoinColumn(name="review_number")
+//    private ReviewEntity dislikedReviewEntity;
+//
+//    @ManyToOne
+//    @JoinColumn(name="user_id")
+//    private UserEntity likedUserEntity;
+//    @ManyToOne
+//    @JoinColumn(name="user_id")
+//    private UserEntity dislikedUserEntity;
+//
+//}
