bf2cf81512461b1a13f98c29a18aacbcf42a8565
unknown <lie224822@gmail.com>
2024-06-28 10:56:36 +0900

    resourceHandler

---
+++ src/main/java/com/example/back/config/MvcConfig.java
@@ -0,0 +1,21 @@
+package com.example.back.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    final Path FILE_ROOT = Paths.get("/app/image").normalize();
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/files/**")
+                .addResourceLocations(FILE_ROOT.toUri().toString());
+    }
+}
